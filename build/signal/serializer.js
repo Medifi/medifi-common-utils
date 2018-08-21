d9d27aad4d47c3915025b6dc89ebe064f8ac60a1
Sieg Collado <siegcollado@gmail.com>
2018-08-21 19:52:48 +0800

    adds compiled version in build/
    - folder is checked in because we don't have npm

---
+++ build/signal/serializer.js
@@ -0,0 +1,80 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getSerializer = undefined;
+
+var _mapping;
+
+var _types = require('./types');
+
+var types = _interopRequireWildcard(_types);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var serializeSignal = function serializeSignal(user) {
+  var dateSent = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+  var payload = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
+  var date = new Date(dateSent).getTime(); // if null, date today is sent
+
+
+  if (!user.id || !user.fullName || !user.role) {
+    throw new Error('expected user to contain id, fullName and role');
+  }
+
+  var data = Object.assign({}, payload, { dateSent: date }, { user: user });
+
+  return JSON.stringify(data);
+};
+
+var message = function message(user, dateSent, _message) {
+  if (!_message.id || !_message.body) {
+    throw new Error('expected message to contain id and body');
+  }
+
+  var id = _message.id,
+      body = _message.body;
+
+  return serializeSignal(user, dateSent, { id: id, body: body });
+};
+
+var typing = function typing(user, dateSent) {
+  var isTyping = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+  return (
+    // cast anything to boolean
+    serializeSignal(user, dateSent, { isTyping: !!isTyping })
+  );
+};
+
+var endSession = function endSession(user, dateSent) {
+  return serializeSignal(user, dateSent, null);
+};
+
+var inviteToVideo = function inviteToVideo(user, dateSent) {
+  return serializeSignal(user, dateSent, null);
+};
+
+var connection = function connection(user, dateSent) {
+  var ready = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+  return serializeSignal(user, dateSent, { ready: !!ready });
+};
+
+var mapping = (_mapping = {}, _defineProperty(_mapping, types.MESSAGE, message), _defineProperty(_mapping, types.INVITE_VIDEO, inviteToVideo), _defineProperty(_mapping, types.END_SESSION, endSession), _defineProperty(_mapping, types.TYPING_INDICATOR, typing), _defineProperty(_mapping, types.CONNECTION, connection), _mapping);
+
+var getSerializer = exports.getSerializer = function getSerializer(type) {
+  return mapping[type];
+};
+
+exports.default = {
+  serializeSignal: serializeSignal,
+  message: message,
+  typing: typing,
+  endSession: endSession,
+  inviteToVideo: inviteToVideo,
+  connection: connection,
+  getSerializer: getSerializer
+};
