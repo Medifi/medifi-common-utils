3a050caa85c3a026940232d517b104d0af93abd2
Sieg Collado <siegcollado@gmail.com>
2018-11-09 13:09:18 +0800

    adds new signal types for rejecting a video invite and ending a video call

--- build/signal/types.js
+++ build/signal/types.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var MESSAGE = exports.MESSAGE = 'message';
-var INVITE_VIDEO = exports.INVITE_VIDEO = 'invite-video';
-var RESPOND_TO_VIDEO_REQUEST = exports.RESPOND_TO_VIDEO_REQUEST = 'respond-video';
+var REJECT_VIDEO_INVITE = exports.REJECT_VIDEO_INVITE = 'reject-video-invite';
+var END_VIDEO_CALL = exports.END_VIDEO_CALL = 'end-video-call';
 var END_SESSION = exports.END_SESSION = 'end-session';
 var TYPING_INDICATOR = exports.TYPING_INDICATOR = 'typing-indicator';
 var CONNECTION = exports.CONNECTION = 'connection';
